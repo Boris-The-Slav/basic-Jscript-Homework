b0913aac71cb2f8ff85b8d59a5ff742f15c76578
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-01-18 23:47:44 +0100

    cleaned up code

--- jscript-hwork-05/app.js
+++ jscript-hwork-05/app.js
@@ -12,9 +12,9 @@ const colorBtn = document.querySelector("#color--btn");
 const tableEl = document.querySelector("#table--element");
 const magicMsg = document.querySelector("#magic--msg");
 
-let allTd = [];
 //create the table funciton
 function createTable() {
+  tableEl.innerHtml = "";
   for (let i = 0; i < Number(rowInput.value) + 1; i++) {
     const newRow = document.createElement("TR");
     for (let j = 0; j < Number(colInput.value) + 1; j++) {
@@ -27,8 +27,8 @@ function createTable() {
         newRow.cells[0].innerText = "TABLE";
       }
     }
+
     newRow.cells[0].innerText = `Row ${i}`;
-    console.log(newRow.cells[0].innerText);
     if (newRow.cells[0].innerText === "Row 0") {
       newRow.cells[0].innerText = `Magical
       Table`;
@@ -38,19 +38,20 @@ function createTable() {
 }
 //add event listener
 createBtn.addEventListener("click", () => {
-  createTable();
+  rowInput.value < 50 && colInput.value < 50
+    ? createTable()
+    : (tableEl.innerHTML =
+        "<h2>THE MAGICAL TABLE WAS SURELY NOT THAT LARGE</h2>");
 });
 
 //create random color function
-
-Math.floor(Math.random() * 256);
-function createRandom(r) {
+function createRandomColor() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`;
 }
-
+//add color event listener
 colorBtn.addEventListener("click", () => {
-  tableEl.style.backgroundColor = createRandom();
+  tableEl.style.backgroundColor = createRandomColor();
 });
