92135623daa6503ea75dac1928eccc0d310da697
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-01-19 08:05:13 +0100

    Update app.js

--- jscript-hwork-05/app.js
+++ jscript-hwork-05/app.js
@@ -17,16 +17,14 @@ function createTable() {
   tableEl.innerHTML = "";
   for (let i = 0; i < Number(rowInput.value) + 1; i++) {
     const newRow = document.createElement("TR");
-    for (let j = 0; j < Number(colInput.value) + 1; j++) {
-      const newCell = document.createElement("TD");
-      newRow.append(newCell);
-    }
+    const cell = "<td></td>";
+    newRow.innerHTML = cell.repeat(Number(colInput.value) + 1);
+    console.log(newRow.cells[0]);
     if (i === 0) {
       for (let g = 0; g <= Number(colInput.value); g++) {
         newRow.cells[g].innerText = `Column ${g}`;
       }
     }
-
     newRow.cells[0].innerText = `Row ${i}`;
     if (newRow.cells[0].innerText === "Row 0") {
       newRow.cells[0].innerText = `Magical
@@ -37,10 +35,13 @@ function createTable() {
 }
 //add event listener
 createBtn.addEventListener("click", () => {
-  rowInput.value < 50 && colInput.value < 50
+  rowInput.value <= 50 &&
+  colInput.value <= 50 &&
+  rowInput.value > 0 &&
+  colInput.value > 0
     ? createTable()
     : (tableEl.innerHTML =
-        "<h2>THE MAGICAL TABLE WAS SURELY NOT THAT LARGE</h2>");
+        "<h2>This realm can only work from 1-50, dont try to cheat.</h2>");
 });
 
 //create random color function
