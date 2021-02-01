d2897b66769b61346f5c5ccdda1399217049fcde
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-02-01 19:51:06 +0100

    Update app.js

--- Jscript-workshop/app.js
+++ Jscript-workshop/app.js
@@ -5,7 +5,7 @@ console.log("working");
 const arrayMap = [0, 2, 5, 4, 6, 8, 10, 12, 3, 4, 4, 3, 4, 4];
 
 function mapNumbers(arr) {
-  let resultString = "";
+  let resultString = `${arrayMap[0]}`;
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] % 2 === 0 && arr[i] % 2 === 0) {
       resultString += `-${arr[i]}`;
@@ -59,26 +59,5 @@ function change(num) {
 
   return resultArr;
 }
-//works with n number
-console.log(change(120));
 
-//trying to make the bonus work
-function changeBonus(num) {
-  const coins = [50, 20, 10, 5, 1];
-  const coinLimits = [4, 8, 10, 20, 30];
-  const resultArr = [];
-  for (let coin of coins)
-    if (num % coin === 0) {
-      for (let i = 0; i < num / coin; i++) {
-        resultArr.push(coin);
-      }
-      break;
-    } else {
-      for (let j = 0; j < (num - (num % coin)) / coin; j++) {
-        resultArr.push(coin);
-      }
-      num = num % coin;
-    }
-
-  return resultArr;
-}
+console.log(change(132));
